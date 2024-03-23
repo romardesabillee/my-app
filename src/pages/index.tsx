73d6ee8a353d8690af798565ce0675178f403794
ChristianDinaVirgin <seanmichael.doinog@hcdc.edu.ph>
2024-03-23 10:39:39 +0800

    homepage updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello  World</h1>
+      <div>
+      <h1>Alawabalo Patotoya!
+      Alawabalo Patotoya!
+      Alawabalo Patotoya!
+      </h1>
+      </div>
     </>
   );
 }
